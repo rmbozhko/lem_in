0fa7a225049ee3d5f6c602317dc82f1162d8902b
rmbozhko <yamakasi.romka@yahoo.fr>
2017-07-11 11:21:49 +0300

    Updating dfs or make it work properly.

--- lem_in_path.c
+++ lem_in_path.c
@@ -1,31 +1,31 @@
-ft_get_room // get real number of the room its index in rooms_arr 
+ #include "lem_in.h"
 
-int			DFS_VISIT(t_ants *farmer, int vertex, int i)
+static int			ft_dfs_visit(t_ants *farmer, int vertex, int i)
 {
 	char		*str;
 
-	if (vertex == (ft_bidlen(farmer->rooms_arr) - 1))
-		return (1);
 	str = farmer->adj_matrix[vertex];
 	while (ft_strchr(str, '1'))
 	{
-		if (!ft_bidchr(farmer->visited_roms, ft_get_room(farmer->rooms_arr[ft_strchr(str, '1') - str])))
+		if (ft_get_rooms_coord(farmer->visited_roms, farmer->rooms_arr[ft_strchr(str, '1') - str]) == -1)
 		{
 			farmer->visited_rooms[i] = farmer->rooms_arr[ft_strchr(str, '1') - str];
-			return (DFS_VISIT(farmer,
+			if (ft_get_rooms_coord(farmer->visited_rooms, farmer->rooms_arr[ft_bidlen(farmer->rooms_arr)]) != -1)
+			    return (1);
+			return (ft_dfs_visit(farmer,
 				ft_get_rooms_coord(farmer->rooms_arr[ft_strchr(str, '1') - str], farmer->rooms_arr), ++i));
 		}
-		str = ft_strchr(str, '1') - str;
+		str = ft_strchr(str, '1') - str + 1; // whether it returns the exact postion or position after the found char.
 	}
 	return (0);
 }
 
-int			DFS(t_ants *farmer)
+int			dfs(t_ants *farmer)
 {
 	if (ft_strchr(farmer->adj_matrix[0], '1'))
 	{
 		farmer->visited_rooms[0] = farmer->rooms_arr[0];
-		return (DFS_VISIT(farmer, 0, 1));
+		return (ft_dfs_visit(farmer, 0, 1));
 	}
 	else
 		return (0);
